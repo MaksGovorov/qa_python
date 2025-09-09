cac3c7f97518bb3ce33f414821f3ade9475b3f5f
Maksim Govorov <ofgovorov@gmail.com>
2025-09-08 21:04:57 -0400

    Добавил фикстуры в отдельный файл и дописал 2 теста для get_books_genre и get_book_genre

--- tests.py
+++ tests.py
@@ -1,37 +1,5 @@
-'''
-# from main import BooksCollector
-
-# # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# # обязательно указывать префикс Test
-# class TestBooksCollector:
-
-#     # пример теста:
-#     # обязательно указывать префикс test_
-#     # дальше идет название метода, который тестируем add_new_book_
-#     # затем, что тестируем add_two_books - добавление двух книг
-#     def test_add_new_book_add_two_books(self):
-#         # создаем экземпляр (объект) класса BooksCollector
-#         collector = BooksCollector()
-
-#         # добавляем две книги
-#         collector.add_new_book('Гордость и предубеждение и зомби')
-#         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-#         # проверяем, что добавилось именно две
-#         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-#         assert len(collector.get_books_rating()) == 2
-
-#     # напиши свои тесты ниже
-#     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-'''
-
-
 import pytest
 from main import BooksCollector
-
-@pytest.fixture
-def collector():
-    return BooksCollector()
 
 
 class TestBooksCollector:
@@ -86,6 +54,12 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Остров сокровищ')
         assert collector.get_list_of_favorites_books().count('Остров сокровищ') == 1
 
+    def test_delete_book_from_favorites(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+        collector.delete_book_from_favorites('Война и мир')
+        assert 'Война и мир' not in collector.get_list_of_favorites_books()
+
 
     @pytest.mark.parametrize(
         'genre, books_list',
@@ -101,3 +75,22 @@ class TestBooksCollector:
         collector.set_book_genre('20 тысяч лье под водой', "Фантастика")
         collector.set_book_genre('Ледокол', "Ужасы")
         assert collector.get_books_with_specific_genre(genre) == books_list
+
+    def test_get_books_genre_returns_dict(self, collector):
+        collector.add_new_book('20 тысяч лье под водой')
+        collector.set_book_genre('20 тысяч лье под водой', 'Фантастика')
+        genres = collector.get_books_genre()
+        assert isinstance(genres, dict)
+        assert genres['20 тысяч лье под водой'] == 'Фантастика'
+
+    def test_get_book_genre_returns_correct_genre(self, collector):
+        collector.add_new_book('Ледокол')
+        collector.set_book_genre('Ледокол', 'Ужасы')
+        genre = collector.get_book_genre('Ледокол')
+        assert genre == 'Ужасы'
+
+    def test_outside_class(self, collector):
+        collector.add_new_book('Тест')
+        collector.set_book_genre('Тест', 'Фантастика')
+        assert collector.get_book_genre('Тест') == 'Фантастика'
+
